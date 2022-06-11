3eeaa7a756f674430dd552bb9a4f269004706a41
Aman__Giri__Emerson7678 <Amangirikg@gmail.com>
2022-06-12 00:11:29 +0530

    5th commit

--- Core Java Project/src/basicJava/DataTypes.java
+++ Core Java Project/src/basicJava/DataTypes.java
@@ -1,8 +1,54 @@
 package basicJava;
 
 public class DataTypes {
+	//Hi this is my first java code
+	
+			/*Hi this 
+			 * is my first java code
+			 * and I am so happy
+			 */
+			
+			//main method --> starting/execution point of the program
+		//duplicates variables are not allowed
 	public static void main(String[] args) {
-		//comment
-		System.out.println("Automation testing");
+		
+		//Primitive data types : int,double,char,boolean
+		
+		//1. int 
+		int i = 10;
+		i=20;
+		int j = 30;
+		System.out.println("the value of the j : "+j);
+		int k = 40;
+		int l = -1;
+		int p = 0;
+		System.out.println(i+j);
+		
+		//2.double
+		double d = 12.33;
+		double d1 = 34.44;
+		double d2 = 100; // 100.00
+		System.out.println(d2);
+		
+		//3.char : only single digit value, should be written within single quotes
+		char c= '1';
+		char c1 = 'A';
+		char c2 = 'a';
+		char c3 = '$';
+		System.out.println(c3);
+		
+		//4.Boolean : 
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		//5.String : is a class, not a data type
+		String s = "Hello World";
+		String s1 = "Selenium";
+		String s2 = "Hi this is my java code";
+		String s3 = "1000";
+		String s4 = "12.33";
+		
+		System.out.println("the value of s2: "+s2);
+		
 	}
 }
