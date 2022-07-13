9af5d29ca8f0998d10a7690d41be9f41d90f09f7
Aman__Giri__Emerson7678 <Amangirikg@gmail.com>
2022-07-14 02:11:18 +0530

    com2

---
+++ Core Java Project/src/basicJava/TernaryOperator.java
@@ -0,0 +1,13 @@
+package basicJava;
+
+public class TernaryOperator {
+	public static void main(String[] args) {
+		// variable = (condition) ? trueExpression : FalseExpression
+		int num1 = 200;
+		int num2 = 30;
+		
+		boolean result = (num1<num2) ? true : false;
+		System.out.println(result);
+		
+	}
+}
