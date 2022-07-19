23255088cac8386834e4f6bcbb37d32a76461f6d
Aman__Giri__Emerson7678 <Amangirikg@gmail.com>
2022-07-20 02:28:55 +0530

    commh

---
+++ Core Java Project/src/oopsByAK/CopyConstructor.java
@@ -0,0 +1,33 @@
+package oopsByAK;
+
+//Copy constructor copies one object and then put it into the another object
+
+class Student3{
+	String name;
+	int age;
+	
+	public void printInfo() {
+		System.out.println(this.name);
+		System.out.println(this.age);
+	}
+	
+	Student3(Student3 s2){ //copy constructor
+		this.name = s2.name;
+		this.age = s2.age;
+	}
+	
+	Student3(){
+		
+	}
+}
+
+public class CopyConstructor {
+	public static void main(String[] args) {
+		Student3 s1 = new Student3();
+		s1.name = "Aman";
+		s1.age = 24;
+		
+		Student3 s2 = new Student3(s1);
+		s2.printInfo();
+	}
+}
--- Core Java Project/src/oopsByAK/ParamtrConstructor.java
+++ Core Java Project/src/oopsByAK/ParamtrConstructor.java
@@ -2,16 +2,23 @@ package oopsByAK;
 
 class Student2{
 	String name;
-	String age;
+	int age;
 	
 	public void printInfo() {
 		System.out.println(this.name);
 		System.out.println(this.age);
 	}
+	
+	Student2(String name,int age){
+		this.name = name;
+		this.age = age;
+	}
 }
 
 public class ParamtrConstructor {
-	public static void main(string[] args) {
-		
+	public static void main(String[] args) {
+		Student2 s1 = new Student2("Aman" , 23);
+		s1.printInfo();
+	
 	}
 }
